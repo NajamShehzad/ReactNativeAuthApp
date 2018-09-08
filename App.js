c5ad11dc4b2394cf566af8a243d746f156d2016f
NajamShehzad <najambutt195@gmail.com>
2018-09-08 21:48:35 +0500

    Import Reusable Components

--- App.js
+++ App.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {Header} from './src/components/index';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View >
+        <Header name={'Authentication'}/>
+        <Text style={styles.wellcomeStyle} >WellCome To React App!</Text>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  wellcomeStyle: {
+    fontSize: 25
+  }
 });
