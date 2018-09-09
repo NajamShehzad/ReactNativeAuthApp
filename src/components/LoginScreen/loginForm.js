e961b33441eea002d1e23794273d2854d486c4ec
NajamShehzad <najambutt195@gmail.com>
2018-09-09 19:52:55 +0500

    Input Field With State Value

--- src/components/LoginScreen/loginForm.js
+++ src/components/LoginScreen/loginForm.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { TextInput } from 'react-native';
 import { Button, Card, CardSection } from '../index';
 
 
 class LoginForm extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            text: ''
+        }
+        this.inputField = this.inputField.bind(this);
+    }
+
+    inputField(text){
+        this.setState({text});
+    }
     render() {
         return (
             <Card>
                 <CardSection>
-
+                    <TextInput
+                        value={this.state.text}
+                        onChangeText={this.inputField}
+                        style={{ height: 40, width: '100%' }}
+                    />
                 </CardSection>
                 <CardSection>
                     <Button>
@@ -19,5 +35,6 @@ class LoginForm extends Component {
         )
     }
 }
+
 
 export default LoginForm;
